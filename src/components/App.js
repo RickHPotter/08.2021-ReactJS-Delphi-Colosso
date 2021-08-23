b09caeade53ddc9b903fbe25ff6ba925ea1696f6
Rick Potter <36988302+RickHPotter@users.noreply.github.com>
2021-08-23 17:25:33 -0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,18 +12,18 @@ import UpdateProfile from "./UpdateProfile"
 function App() {
   return (
     <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
+      className = "d-flex align-items-center justify-content-center"
+      style = { { minHeight: "100vh" } }
     >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
+      <div className = "w-100" style = { { maxWidth: "400px" } } >
         <Router>
           <AuthProvider>
             <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
+              <PrivateRoute exact path = "/" component = { Dashboard } />
+              <PrivateRoute path = "/update-profile" component = { UpdateProfile } />
+              <Route path = "/signup" component = { Signup } />
+              <Route path = "/login" component = { Login } />
+              <Route path = "/forgot-password" component = { ForgotPassword } />
             </Switch>
           </AuthProvider>
         </Router>
