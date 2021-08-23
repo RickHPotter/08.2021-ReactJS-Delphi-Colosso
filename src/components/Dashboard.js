c94f56b6f0dd6ffa5a3a27d49e08d1ab18fe3346
Rick Potter <36988302+RickHPotter@users.noreply.github.com>
2021-08-23 17:24:17 -0300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -23,16 +23,16 @@ export default function Dashboard() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+          <h2 className = "text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger"> { error } </Alert>}
+          <strong>Email:</strong> { currentUser.email }
+          <Link to = "/update-profile" className = "btn btn-primary w-100 mt-3">
             Update Profile
           </Link>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
+      <div className = "w-100 text-center mt-2">
+        <Button variant = "link" onClick = { handleLogout } >
           Log Out
         </Button>
       </div>
